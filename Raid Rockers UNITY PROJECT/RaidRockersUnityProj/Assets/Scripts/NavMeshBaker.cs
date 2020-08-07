ffc87056e63d32ad6096f028d4099306157921b7
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-08-06 23:13:31 -0400

    Learned about Costs and Baking Nav Meshes in real time

---
+++ Raid Rockers UNITY PROJECT/RaidRockersUnityProj/Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// A collection of the Nav Mesh Surfaces this tries to get.
+    /// </summary>
+    [SerializeField]
+    NavMeshSurface navMeshSurface;
+    #endregion
+
+    void Start()
+    {
+        navMeshSurface.BuildNavMesh();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
