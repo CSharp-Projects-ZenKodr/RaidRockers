eaae767acc8a0824173b35fcac9749e653f38c77
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-08-09 22:51:53 -0400

    Created custom cursor for game

---
+++ Raid Rockers UNITY PROJECT/RaidRockersUnityProj/Assets/Scripts/PlayerCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// Return true to show the cursor during gameplay, or false if not.
+    /// </summary>
+    [Tooltip("Return true to show the cursor during gameplay, or false if not.  Debugging purposes only.")]
+    public bool showCursor = false;
+    //[Space(8)]
+    #endregion
+
+    private void Start()
+    {
+        Cursor.visible = showCursor;
+    }
+
+    void Update()
+    {
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(cursorPos);
+        transform.position = Input.mousePosition;
+    }
+}
