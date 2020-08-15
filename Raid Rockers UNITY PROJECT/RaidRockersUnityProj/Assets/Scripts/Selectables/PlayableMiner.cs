a4b8c9f9518dae3b071d220b7704ab0f6990692e
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-08-15 19:20:12 -0400

    Click Validation complete, Miner code refactored

--- Raid Rockers UNITY PROJECT/RaidRockersUnityProj/Assets/Scripts/Selectables/PlayableMiner.cs
+++ Raid Rockers UNITY PROJECT/RaidRockersUnityProj/Assets/Scripts/Selectables/PlayableMiner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -40,48 +41,86 @@ public class PlayableMiner : SelectableObject
     //Instead of immediately selecting another object, we want to move to where we click
     private void Update()
     {
-        MinerMovement();
+        InteractWithAClick();
     }
 
-    void MinerMovement()
+    /// <summary>
+    /// The function that handles what happens when the player clicks something in the environment when a miner is selected.
+    /// </summary>
+    void InteractWithAClick()
     {
-        //Todo: make sure the place we're clicking and moving to is valid (not a wall, on the NavMesh)
-        if (Input.GetMouseButtonDown(0) && selected && currentHoveredObject != null && currentHoveredObject.objectName == "GROUND")
+        if (Input.GetMouseButtonDown(0) && selected)
         {
-            //Moving the miner to a point on the ground that I clicked.
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            bool didRayHit = Physics.Raycast(ray, out hit);
+            if (currentHoveredObject == null)
+            {
 
-            if (didRayHit)
-            {
-                if (hit.collider.gameObject != gameObject)
-                {
-                    //Allow the miner to move again
-                    attachedAgent.isStopped = false;
-                    //Set it's destination to where I clicked
-                    attachedAgent.SetDestination(hit.point);
-                    //Deselect the miner
-                    DeselectPreviousObject();
-                    selectAfterMouseDown = true;
-                    aMinerIsSelected = false;
-                    //Tell the cursor the miner was deselected
-                    cursorAnimator.SetTrigger("Check");
-                    cursorAnimator.ResetTrigger("Move");
-                }
-            }
-            else
-            {
                 //TODO: When the player clicks, the cross does play, but the cursor overrides it and goes default
-                Debug.Log("Don't move");
+                Debug.Log(name + " will do nothing.", gameObject);
+                DeselectMiner();
+                //Animate cursor
+                cursorAnimator.ResetTrigger("Move");
+                cursorAnimator.SetTrigger("Cross");
+                return;
+            }
+
+            switch(currentHoveredObject.objectName)
+            {
+                case "GROUND":
+                    MinerMoveCommand();
+                    break;
+                case "MINER":
+                    //Do nothing, maybe play check anim
+                    cursorAnimator.ResetTrigger("Move");
+                    cursorAnimator.SetTrigger("Check");
+                    break;
+                default:
+                    Debug.Log(name + " will do nothing.", gameObject);
+                    DeselectMiner();
+                    //Animate cursor
+                    cursorAnimator.ResetTrigger("Move");
+                    cursorAnimator.SetTrigger("Cross");
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// The command that tells the miner to move where player clicked
+    /// </summary>
+    private void MinerMoveCommand()
+    {
+        //Moving the miner to a point on the ground that I clicked.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool didRayHit = Physics.Raycast(ray, out hit);
+
+        if (didRayHit)
+        {
+            if (hit.collider.gameObject != gameObject)
+            {
+                //Allow the miner to move again
+                attachedAgent.isStopped = false;
+                //Set it's destination to where I clicked
+                attachedAgent.SetDestination(hit.point);
                 //Deselect the miner
                 DeselectPreviousObject();
                 selectAfterMouseDown = true;
                 aMinerIsSelected = false;
-                //Animate cursor
+                //Tell the cursor the miner was deselected
+                cursorAnimator.SetTrigger("Check");
                 cursorAnimator.ResetTrigger("Move");
-                cursorAnimator.SetTrigger("Cross");
             }
         }
+    }
+
+    /// <summary>
+    /// Deselects the miner.
+    /// </summary>
+    void DeselectMiner()
+    {
+        //Deselect the miner
+        DeselectPreviousObject();
+        selectAfterMouseDown = true;
+        aMinerIsSelected = false;
     }
 }
